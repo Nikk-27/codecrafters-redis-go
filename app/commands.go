397eb6233126c623bc749b0018201d0b961293b7
Nikk-27 <nktllwn@gmail.com>
2024-11-20 13:04:09 -0800

    implement ECHO command using Redis protocol parser

--- app/commands.go
+++ app/commands.go
@@ -13,3 +13,11 @@ func echo(args []Value) Value {
     }
     return Value{typ: "string", bulk: args[0].bulk}
 }
+
+// echo function to handle the ECHO command
+func ping(args []Value) Value {
+    if len(args) != 1 {
+        return Value{typ: "error", bulk: "ERR wrong number of arguments for 'echo' command"}
+    }
+    return Value{typ: "string", bulk: args[0].bulk}
+}
