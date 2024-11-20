357d885cdc986e064ac06c1d14f84e0e725f948f
Nikk-27 <nktllwn@gmail.com>
2024-11-20 00:12:51 -0800

    implement ECHO command using Redis protocol parser

---
+++ multiple_clients.go
@@ -0,0 +1,47 @@
+package main
+import (
+	"fmt"
+	"net"
+	"os"
+)
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	// Uncomment this block to pass the first stage
+	//
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer listener.Close()
+	// defer func() {
+	// 	listener.Close()
+	// }()
+
+	fmt.Println("Listening on " + listener.Addr().String())
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+		}
+		fmt.Println("Accepted connection from " + conn.RemoteAddr().String())
+		go handleConnection(conn)
+	}
+}
+func handleConnection(conn net.Conn) {
+	defer func() {
+		conn.Close()
+	}()
+
+	message := []byte("+PONG\r\n")
+	fmt.Println("Handling connection")
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Failed to read data")
+			return
+		}
+		conn.Write(message)
+	}
+}
